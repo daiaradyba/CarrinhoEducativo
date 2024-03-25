ae3409832dc0b011b2dbf05243d3609397e6c3f2
Daiara Dyba <daiaradyba@gmail.com>
2024-03-25 16:22:43 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var workspace = Blockly.inject('blocklyDiv',
+    {toolbox: document.getElementById('toolbox')});
+
+function showCode() {
+    // Gera o código a partir dos blocos
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    alert(code);
+}
