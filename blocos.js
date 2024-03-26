adf9a60bfad56afe1e2986506d029359474b88ea
Daiara Dyba <daiaradyba@gmail.com>
2024-03-26 13:45:26 -0300

    Frente, tras, esquerda, direita

--- blocos.js
+++ blocos.js
@@ -1,17 +1,95 @@
-Blockly.Blocks['ir_para_frente'] = {
+Blockly.Blocks['frente'] = {
     init: function() {
-      this.appendValueInput("frente_unidades")
+      this.appendValueInput("valor_frente")
           .setCheck("Number")
-          .appendField("IR_PRA_FRENTE_POR");
-      this.setColour(65);
+          .setAlign(Blockly.ALIGN_RIGHT)
+          .appendField("Ir para frente por");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+  Blockly.Blocks['tras'] = {
+    init: function() {
+      this.appendValueInput("valor_tras")
+          .setCheck("Number")
+          .setAlign(Blockly.ALIGN_RIGHT)
+          .appendField("Ir para tras por");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
    this.setTooltip("");
    this.setHelpUrl("");
     }
   };
 
-  javascript.javascriptGenerator.forBlock['ir_para_frente'] = function(block, generator) {
-    var value_frente_unidades = generator.valueToCode(block, 'frente_unidades', javascript.Order.ATOMIC);
+  Blockly.Blocks['esquerda'] = {
+    init: function() {
+      this.appendValueInput("valor_esquerda")
+          .setCheck("Number")
+          .setAlign(Blockly.ALIGN_RIGHT)
+          .appendField("Virar a esquerda por");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
 
-    var code = 'frente:' + value_frente_unidades + '\n';
+  Blockly.Blocks['direita'] = {
+    init: function() {
+      this.appendValueInput("valor_direita")
+          .setCheck("Number")
+          .setAlign(Blockly.ALIGN_RIGHT)
+          .appendField("Virar a direita por");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  javascript.javascriptGenerator.forBlock['esquerda'] = function(block, generator) {
+    var value_valor_esquerda = generator.valueToCode(block, 'valor_esquerda', javascript.Order.ATOMIC);
+    var code = 'esquerda:'+value_valor_esquerda+'\n';
+    return code;
+  };
+
+  javascript.javascriptGenerator.forBlock['direita'] = function(block, generator) {
+    var value_valor_direita = generator.valueToCode(block, 'valor_direita', javascript.Order.ATOMIC);
+    var code = 'direita:'+value_valor_direita+'\n';
+    return code;
+  };
+
+  javascript.javascriptGenerator.forBlock['frente'] = function(block, generator) {
+    var value_valor_frente = generator.valueToCode(block, 'valor_frente', javascript.Order.ATOMIC);
+    var code = 'frente:'+value_valor_frente+'\n';
+    return code;
+  };
+
+  javascript.javascriptGenerator.forBlock['tras'] = function(block, generator) {
+    var value_valor_tras = generator.valueToCode(block, 'valor_tras', javascript.Order.ATOMIC);
+    var code = 'tras:'+value_valor_tras+'\n';
+    return code;
+  };
+
+  Blockly.Blocks['start'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("Início");
+      this.setNextStatement(true, null);
+      this.setColour(330);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  javascript.javascriptGenerator.forBlock['start'] = function(block, generator) {
+    // TODO: Assemble javascript into code variable.
+    var code = 'start\n';
     return code;
   };
