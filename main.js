945cb3189466005141814c2b6812d123e688fbf9
Daiara Dyba <daiaradyba@gmail.com>
2024-03-26 15:11:27 -0300

    salvar workspace lvl1

--- main.js
+++ main.js
@@ -23,9 +23,19 @@ function render() {
   } else if (appState === 'tela2') {
     // Conteúdo da Tela 2
     appContainer.innerHTML = '<div class="container"><div id="blocklyDiv"></div></div><xml id="toolbox" style="display: none"><category name="Controles Carro" colour="#F33D49"><block type="start"></block><block type="frente"></block><block type="tras"></block><block type="esquerda"></block><block type="direita"></block></category><category name="Lógica" colour="#774AD9"><block type="controls_if"></block><block type="logic_compare"></block><block type="logic_operation"></block><block type="logic_negate"></block><block type="logic_boolean"></block></category><category name="Laços" colour="#5ED905"><block type="controls_repeat_ext"><value name="TIMES"><block type="math_number"><field name="NUM">10</field></block></value></block><block type="controls_whileUntil"></block></category><category name="Matemática" colour="#E80C7A"><block type="math_number"></block><block type="math_arithmetic"></block></category><!-- Continue adicionando categorias e blocos conforme necessário --></xml><button id="showCode">Gerar Código JS</button>';
+    
     var workspace = Blockly.inject('blocklyDiv', {
       toolbox: document.getElementById('toolbox')
     });
+   
+    if (window.localStorage) {
+      const xmlText = localStorage.getItem('blocklyWorkspace_level1');
+      if (xmlText) {
+        const xml = Blockly.utils.xml.textToDom(xmlText);
+        Blockly.Xml.domToWorkspace(xml, workspace);
+      }
+    }
+    console.log("carreguei\n");
 
     const buttonTela1 = document.createElement('button');
     buttonTela1.innerText = 'Ir para a Tela 1';
@@ -39,6 +49,13 @@ function render() {
 
     buttonTela1.onclick = function() {
       appState = 'tela1';
+      // Salvar workspace
+      if (window.localStorage) {
+        const xml = Blockly.Xml.workspaceToDom(workspace);
+        const xmlText = Blockly.utils.xml.domToText(xml);
+        localStorage.setItem('blocklyWorkspace_level1', xmlText);
+      }
+
       render(); // Renderiza novamente com o novo estado
     };
     appContainer.appendChild(buttonTela1);
