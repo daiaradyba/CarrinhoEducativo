ab2890d47cae12ef1d315ff5e2fd0e3e56870d2f
Daiara Dyba <daiaradyba@gmail.com>
2024-03-27 13:31:36 -0300

    Tentativa bug chrome android

--- level.js
+++ level.js
@@ -1,9 +1,8 @@
 class Level {
-    constructor(name, xmlWorkspaceKey, toolboxXml, contentHtml,app,instructionsImg) {
+    constructor(name, xmlWorkspaceKey, toolboxXml,app,instructionsImg) {
       this.name = name;
       this.xmlWorkspaceKey = xmlWorkspaceKey;
       this.toolboxXml = toolboxXml;
-      this.contentHtml = contentHtml;
       this.workspace = null; // Blockly workspace será inicializado depois
       this.app = app;
       this.instructionsImg = instructionsImg;
@@ -12,6 +11,7 @@ class Level {
     // Método para inicializar o Blockly workspace
     initBlockly() {
       const toolbox = document.getElementById('toolbox');
+      console.log(toolbox);
       toolbox.innerHTML = this.toolboxXml;
       this.workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox });
   
@@ -22,71 +22,98 @@ class Level {
         Blockly.Xml.domToWorkspace(xml, this.workspace);
       }
     }
-      // Renderiza instruções.
+      /* // Renderiza instruções.
       render(appContainer) {
-        appContainer.innerHTML = '';
-          // Limpa e adiciona o conteúdo, incluindo o botão com um ID específico
-        appContainer.innerHTML = `
-        <div class="instructions-container">
-            <img src="${this.instructionsImg}" alt="Instructions" class="instructions-img">
-        </div>`;
-      
-        const buttonstart = document.createElement('button');
-        buttonstart.innerText = 'Iniciar Level';
-        buttonstart.className = 'button';
-
-        buttonstart.onclick = () => {
-            this.startLevel(appContainer);
-        };
-
-        appContainer.appendChild(buttonstart);
+        
+       
     
 
     }
-
+ */
 //Antigo Render
-        startLevel(appContainer){
-          
-            appContainer.innerHTML = this.contentHtml;
+render(appContainer) {
+    // Primeiro, limpa o conteúdo atual do appContainer
+    appContainer.innerHTML = '';
 
-        const showCode = document.createElement('button');
-        showCode.innerText = 'Gerar JavaScript';
-        showCode.className = 'button';
+    // Cria o container das instruções e o botão de iniciar
+    const instructionsContainer = document.createElement('div');
+    instructionsContainer.className = 'instructions-container';
+    instructionsContainer.innerHTML = `<img src="${this.instructionsImg}" alt="Instructions" class="instructions-img">`;
 
-        const buttonMenu = document.createElement('button');
-        buttonMenu.innerText = 'Menu';
-        buttonMenu.className = 'button';
-        
-        
-    
-        const buttonRessetarLevel = document.createElement('button');
-        buttonRessetarLevel.innerText = 'Ressetar Level';
-        buttonRessetarLevel.className = 'button';
-        
-    
-        showCode.onclick =  function() {
+    const buttonStart = document.createElement('button');
+    buttonStart.innerText = 'Iniciar Level';
+    buttonStart.className = 'button start-level-btn';
+
+    // Adiciona o container de instruções e o botão ao appContainer
+    appContainer.appendChild(instructionsContainer);
+    instructionsContainer.appendChild(buttonStart);
+
+    // Cria e configura os outros elementos (Blockly, Menu, Reset, Show Code)
+    // mas os deixa ocultos inicialmente
+    const blocklyDiv = document.createElement('div');
+    blocklyDiv.id = 'blocklyDiv';
+    blocklyDiv.style.display = 'none'; // Oculta inicialmente
+
+    const buttonMenu = document.createElement('button');
+    buttonMenu.innerText = 'Menu';
+    buttonMenu.className = 'button';
+    buttonMenu.style.display = 'none'; // Oculta inicialmente
+
+    const buttonRessetarLevel = document.createElement('button');
+    buttonRessetarLevel.innerText = 'Ressetar Level';
+    buttonRessetarLevel.className = 'button';
+    buttonRessetarLevel.style.display = 'none'; // Oculta inicialmente
+
+    const showCode = document.createElement('button');
+    showCode.innerText = 'Gerar JavaScript';
+    showCode.className = 'button';
+    showCode.style.display = 'none'; // Oculta inicialmente
+
+    // Cria o elemento xml para o toolbox do Blockly
+    const toolbox = document.createElement('xml');
+    toolbox.id = 'toolbox';
+    toolbox.style.display = 'none';
+
+    // Agora você pode anexar esses elementos ao appContainer ou a qualquer outro contêiner relevante
+   
+
+    // Define os eventos dos botões
+    showCode.onclick = () => {
         var code = Blockly.JavaScript.workspaceToCode(this.workspace);
         console.log(code);
         alert(code);
-        }
+    };
     
-        buttonRessetarLevel.onclick = () => {
-            this.workspace.clear();
-        };
+    buttonRessetarLevel.onclick = () => {
+        this.workspace.clear();
+    };
     
-        buttonMenu.onclick = () => {
-            this.app.changeLevel('menu');
-            this.app.render(); // acho que to renderizando duas vezes. tentar tirar e ver oq acontece
-        };
-        
-        appContainer.appendChild(buttonMenu);
-        appContainer.appendChild(showCode);
-        appContainer.appendChild(buttonRessetarLevel);
-        appContainer.appendChild(blocklyDiv);
-    
-      this.initBlockly();
-    
-        }
+    buttonMenu.onclick = () => {
+        this.app.changeLevel('menu');
+    };
+
+    appContainer.appendChild(blocklyDiv);
+    appContainer.appendChild(buttonMenu);
+    appContainer.appendChild(buttonRessetarLevel);
+    appContainer.appendChild(showCode);
+    appContainer.appendChild(blocklyDiv);
+    appContainer.appendChild(toolbox);
+
+    // Inicializa o Blockly (oculto inicialmente)
+   
+
+    // Define o evento do botão de iniciar para ocultar as instruções
+    // e mostrar os outros elementos
+    buttonStart.onclick = () => {
+       
+        instructionsContainer.style.display = 'none'; // Oculta as instruções
+        blocklyDiv.style.display = 'block'; // Mostra Blockly
+        buttonMenu.style.display = 'inline-block'; // Mostra Menu
+        buttonRessetarLevel.style.display = 'inline-block'; // Mostra Reset
+        showCode.style.display = 'inline-block'; // Mostra Show Code
+        this.initBlockly();
+    };
+}
     
 
     // Método para salvar o estado do Blockly workspace
--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const level1 = new Level(
   'tela1',
   'blocklyWorkspace_level1',
   '<category name="Controles Carro lvl 1 " colour="#F33D49"><block type="start"></block><block type="frente"></block><block type="tras"></block><block type="esquerda"></block><block type="direita"></block></category><category name="Lógica" colour="#774AD9"><block type="controls_if"></block><block type="logic_compare"></block><block type="logic_operation"></block><block type="logic_negate"></block><block type="logic_boolean"></block></category><category name="Laços" colour="#5ED905"><block type="controls_repeat_ext"><value name="TIMES"><block type="math_number"><field name="NUM">10</field></block></value></block><block type="controls_whileUntil"></block></category><category name="Matemática" colour="#E80C7A"><block type="math_number"></block><block type="math_arithmetic"></block></category><!-- Continue adicionando categorias e blocos conforme necessário -->',
-  '<div id="blocklyDiv"></div><xml id="toolbox" style="display: none"></xml>',
   app,
   '1.png'
 );
@@ -13,16 +12,14 @@ const level2 = new Level(
   'tela2',
   'blocklyWorkspace_level2',
   '<category name="Controles Carro lvl 2" colour="#F33D49"><block type="start"></block><block type="frente"></block><block type="tras"></block><block type="esquerda"></block><block type="direita"></block></category><category name="Lógica" colour="#774AD9"><block type="controls_if"></block><block type="logic_compare"></block><block type="logic_operation"></block><block type="logic_negate"></block><block type="logic_boolean"></block></category><category name="Laços" colour="#5ED905"><block type="controls_repeat_ext"><value name="TIMES"><block type="math_number"><field name="NUM">10</field></block></value></block><block type="controls_whileUntil"></block></category><category name="Matemática" colour="#E80C7A"><block type="math_number"></block><block type="math_arithmetic"></block></category><!-- Continue adicionando categorias e blocos conforme necessário -->',
-  '<div id="blocklyDiv"></div><xml id="toolbox" style="display: none"><category name="Controles Carro" colour="#F33D49"><block type="start"></block><block type="frente"></block><block type="tras"></block><block type="esquerda"></block><block type="direita"></block></category><category name="Lógica" colour="#774AD9"><block type="controls_if"></block><block type="logic_compare"></block><block type="logic_operation"></block><block type="logic_negate"></block><block type="logic_boolean"></block></category><category name="Laços" colour="#5ED905"><block type="controls_repeat_ext"><value name="TIMES"><block type="math_number"><field name="NUM">10</field></block></value></block><block type="controls_whileUntil"></block></category><category name="Matemática" colour="#E80C7A"><block type="math_number"></block><block type="math_arithmetic"></block></category><!-- Continue adicionando categorias e blocos conforme necessário --></xml>',
   app,
   '2.png'
   );
 
 const level3 = new Level(
-    'tela3d',
+    'tela3e',
     'blocklyWorkspace_level3',
     '<category name="Controles Carro lvl 3" colour="#F33D49"><block type="start"></block><block type="frente"></block><block type="tras"></block><block type="esquerda"></block><block type="direita"></block></category><category name="Lógica" colour="#774AD9"><block type="controls_if"></block><block type="logic_compare"></block><block type="logic_operation"></block><block type="logic_negate"></block><block type="logic_boolean"></block></category><category name="Laços" colour="#5ED905"><block type="controls_repeat_ext"><value name="TIMES"><block type="math_number"><field name="NUM">10</field></block></value></block><block type="controls_whileUntil"></block></category><category name="Matemática" colour="#E80C7A"><block type="math_number"></block><block type="math_arithmetic"></block></category><!-- Continue adicionando categorias e blocos conforme necessário -->',
-    '<div id="blocklyDiv"></div><xml id="toolbox" style="display: none"><category name="Controles Carro" colour="#F33D49"><block type="start"></block><block type="frente"></block><block type="tras"></block><block type="esquerda"></block><block type="direita"></block></category><category name="Lógica" colour="#774AD9"><block type="controls_if"></block><block type="logic_compare"></block><block type="logic_operation"></block><block type="logic_negate"></block><block type="logic_boolean"></block></category><category name="Laços" colour="#5ED905"><block type="controls_repeat_ext"><value name="TIMES"><block type="math_number"><field name="NUM">10</field></block></value></block><block type="controls_whileUntil"></block></category><category name="Matemática" colour="#E80C7A"><block type="math_number"></block><block type="math_arithmetic"></block></category><!-- Continue adicionando categorias e blocos conforme necessário --></xml>',
     app,
     '3.png'
     );  
