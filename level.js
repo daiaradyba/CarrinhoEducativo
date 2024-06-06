98459a3ce2349514fc37a13ca52724ac7750bd74
Daiara Dyba <daiaradyba@gmail.com>
2024-06-06 12:03:10 -0300

    mod1 completo

--- level.js
+++ level.js
@@ -128,6 +128,62 @@ render(appContainer) {
     appContainer.style.backgroundColor = cor_fundo_blocky;
     document.body.style.backgroundColor = cor_fundo_blocky;
 
+    const topo_alert = document.createElement('div');
+    topo_alert.className = 'topo';
+    topo_alert.style.justifyContent = 'center';
+    topo_alert.style.display = 'none';
+
+        // Criar o container principal
+    const successDiv = document.createElement('div');
+    successDiv.className = 'success';
+
+    // Criar o ícone de sucesso
+    const successIconDiv = document.createElement('div');
+    successIconDiv.className = 'success__icon';
+    const successIconSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    successIconSVG.setAttribute('fill', 'none');
+    successIconSVG.setAttribute('height', '24');
+    successIconSVG.setAttribute('viewBox', '0 0 24 24');
+    successIconSVG.setAttribute('width', '24');
+    const successIconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    successIconPath.setAttribute('clip-rule', 'evenodd');
+    successIconPath.setAttribute('d', 'm12 1c-6.075 0-11 4.925-11 11s4.925 11 11 11 11-4.925 11-11-4.925-11-11-11zm4.768 9.14c.0878-.1004.1546-.21726.1966-.34383.0419-.12657.0581-.26026.0477-.39319-.0105-.13293-.0475-.26242-.1087-.38085-.0613-.11844-.1456-.22342-.2481-.30879-.1024-.08536-.2209-.14938-.3484-.18828s-.2616-.0519-.3942-.03823c-.1327.01366-.2612.05372-.3782.1178-.1169.06409-.2198.15091-.3027.25537l-4.3 5.159-2.225-2.226c-.1886-.1822-.4412-.283-.7034-.2807s-.51301.1075-.69842.2929-.29058.4362-.29285.6984c-.00228.2622.09851.5148.28067.7034l3 3c.0983.0982.2159.1748.3454.2251.1295.0502.2681.0729.4069.0665.1387-.0063.2747-.0414.3991-.1032.1244-.0617.2347-.1487.3236-.2554z');
+    successIconPath.setAttribute('fill', '#393a37');
+    successIconPath.setAttribute('fill-rule', 'evenodd');
+    successIconSVG.appendChild(successIconPath);
+    successIconDiv.appendChild(successIconSVG);
+
+    // Criar o título
+    const successTitleDiv = document.createElement('div');
+    successTitleDiv.className = 'success__title';
+    successTitleDiv.textContent = 'Código enviado. Aperte o botão de reset no carrinho';
+
+    // Criar o botão de fechar
+    const successCloseDiv = document.createElement('div');
+    successCloseDiv.className = 'success__close';
+    const successCloseSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    successCloseSVG.setAttribute('height', '20');
+    successCloseSVG.setAttribute('viewBox', '0 0 20 20');
+    successCloseSVG.setAttribute('width', '20');
+    const successClosePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    successClosePath.setAttribute('d', 'm15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z');
+    successClosePath.setAttribute('fill', '#393a37');
+    successCloseSVG.appendChild(successClosePath);
+    successCloseDiv.appendChild(successCloseSVG);
+
+    // Montar o container principal
+    successDiv.appendChild(successIconDiv);
+    successDiv.appendChild(successTitleDiv);
+    successDiv.appendChild(successCloseDiv);
+
+    // Adicionar o container ao corpo do documento
+    topo_alert.appendChild(successDiv);
+    appContainer.appendChild(topo_alert);
+
+    successCloseDiv.onclick = function() {
+      topo_alert.style.display = 'none';
+    };
+
    this.atualizaVariaveis();
 
    const topo = document.createElement('div');
@@ -547,7 +603,7 @@ render(appContainer) {
         const code = Blockly.JavaScript.workspaceToCode(this.workspace);
         console.log(code); // Para depuração
 
-        
+        topo_alert.style.display = 'flex';
         database.ref("/").update({
             code: code
           });
